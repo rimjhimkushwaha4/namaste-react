ed3564266b25e03a94ace548d314bdfe8f60afcf
rimjhim-webiwork <rimjhim@webiwork.com>
2024-09-04 15:20:48 +0930

    episode 1

---
+++ App.js
@@ -0,0 +1,4 @@
+const heading = React.createElement('div',{}, React.createElement('h1',{},'hii'))
+const root=ReactDOM.createRoot(document.getElementById('root'))
+root.render(heading)
+console.log(heading)
